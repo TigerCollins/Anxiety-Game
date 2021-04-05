ac36072ecbd785502b580f33ce9b41bbcdd70dc9
Tiger <tiger.collins@outlook.com>
2021-04-05 22:09:22 +1000

    Implemented horizontal movement. Camera follows player.  Raycasting implemented.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cameraScript;
+    [SerializeField]
+    private GameObject cameraObject;
+    [SerializeField]
+    private Transform cameraTarget;
+    [SerializeField]
+    private CharacterController targetObject;
+
+    [Header("Camera Movement Settings")]
+    [Tooltip("The lower the number, the shorter the delay. 0 for instant follow.")]
+    [SerializeField]
+    private float cameraDelay;
+    [SerializeField]
+    private float xOffset;
+    [SerializeField]
+    private float yOffset;
+    [SerializeField]
+    private float zOffset;
+    private Vector3 velocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //velocity = targetObject.velocity; //current velocity
+        cameraObject.transform.position = Vector3.SmoothDamp(cameraObject.transform.position, new Vector3(cameraTarget.position.x + xOffset, cameraTarget.position.y + yOffset, cameraTarget.position.z + zOffset),ref velocity, cameraDelay);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerObject;
+    [SerializeField]
+    private CharacterController characterCollider;
+    [Header("Movement")]
+    [SerializeField]
+    private float gravityForce;
+    [SerializeField]
+    private float movementMultiplier;
+    [SerializeField]
+    private Vector2 moveAxis;
+    private Vector3 moveVector;
+    [SerializeField]
+    private Vector3 desiredMoveDirection;
+    [SerializeField]
+    private float maxMovementSpeed = 1;
+
+    [Header("Raycast")]
+    public Transform rayCastingPoint;
+    public float raycastDistance;
+    public Color raycastColour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+        Raycast();
+    }
+
+    //Gets reference from Player Input script
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveAxis = context.ReadValue<Vector2>();
+    }
+
+
+    public void Movement()
+    {
+        //Sets gravity for player
+        moveVector = new Vector3(0, gravityForce * .2f * Time.deltaTime, 0);
+        characterCollider.Move(moveVector);
+
+        //Sets up movement and it from OnMove function
+        float horizontal = moveAxis.x * movementMultiplier;
+        desiredMoveDirection = Vector3.ClampMagnitude(new Vector3(horizontal, 0, 0), maxMovementSpeed);
+        characterCollider.Move(desiredMoveDirection * Time.deltaTime * movementMultiplier);
+
+    }
+
+    void Raycast()
+    {
+        // The raycast
+        RaycastHit hit;
+        Debug.DrawRay(rayCastingPoint.position, characterCollider.transform.forward * raycastDistance, raycastColour, Time.deltaTime);
+
+        //Checks a raycast for scripts with the If statement
+        if (Physics.Raycast(rayCastingPoint.position, characterCollider.transform.forward, out hit, raycastDistance))
+        {
+        }
+    }
+}
